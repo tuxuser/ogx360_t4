dc44d19d85e6dc0d896c814d204a916b5192019c
tuxuser <462620+tuxuser@users.noreply.github.com>
2020-11-29 00:12:27 +0100

    Duh, fix typo - thx @Ryzee119

--- src/usbd/diskio_wrapper.h
+++ src/usbd/diskio_wrapper.h
@@ -16,7 +16,7 @@ extern "C"
     uint32_t _disk_volume_get_block_size();
     uint32_t _disk_volume_get_cluster_size();
     uint8_t _read_block(uint32_t block, uint8_t *buf);
-    uint8_t _write_block(uint32_t block, const void *buf);
+    uint8_t _write_block(uint32_t block, const uint8_t *buf);
 
 #ifdef __cplusplus
 }
